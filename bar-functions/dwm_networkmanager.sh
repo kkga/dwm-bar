10e242ee45141ba62eb80cfb00d5be5cf0b96fe3
Gadzhi Kharkharov <me@kkga.me>
2020-09-24 11:31:26 +0300

    upd

--- bar-functions/dwm_networkmanager.sh
+++ bar-functions/dwm_networkmanager.sh
@@ -10,8 +10,6 @@ dwm_networkmanager () {
     CONNAME=$(nmcli -a | grep 'Wired connection' | awk 'NR==1{print $1}' | cut -c 1-7)
     if [ "$CONNAME" = "" ]; then
         CONNAME=$(nmcli -t -f active,ssid dev wifi | grep '^yes' | cut -c 5-)
-    else
-        CONNAME="ETH"
     fi
 
     PRIVATE=$(nmcli -a | grep 'inet4 192' | awk '{print $2}')
